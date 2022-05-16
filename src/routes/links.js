53639f77c25902a329bbf45b710ac94534581d8a
AnaAlmeda <anasol03.89@gmail.com>
2022-05-16 17:34:23 -0300

    segundo commit

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,10 +1,15 @@
 const express =  require('express');
+const res = require('express/lib/response');
 const router = express.Router();
 
 const connection = require('../database');
 
 router.get('/add', (req, res) => {
     res.render('links/add');
+});
+
+router.post('/add',(req,resp)=> {
+    res.send('recibido');
 });
 
 
